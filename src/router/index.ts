25cfb5652760716519792081898fa1fc911a0fb6
Jose Guzman <joseguzmandev@gmail.com>
2023-01-26 12:10:00 -0500

    Se configuro prettier y eslint en el proyecto con reglas basicas

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,18 +1,26 @@
 import { Router } from 'express';
+import AuthRouter from './auth.routes';
 
 export default class RouterAPI {
     public router: Router;
+    // add more routes here
+    private authRouter: AuthRouter;
 
     constructor() {
         this.router = Router();
-
+        // add more routes here
+        this.authRouter = new AuthRouter();
 
         this.initializeRoutes(
+            this.authRouter
         );
     }
 
     private initializeRoutes(
+        // add more routes here
+        authRouter: AuthRouter
     ) {
+        this.router.use('/auth', authRouter.router);
     }
 }
 
