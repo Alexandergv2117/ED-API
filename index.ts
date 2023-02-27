ca9529a45780551fa22575f4db21eff857781d6f
Jose Guzman <joseguzmandev@gmail.com>
2023-02-27 15:22:05 -0500

    Se agregaron validaciones en caso de que el puerto este en uso

--- index.ts
+++ index.ts
@@ -1,17 +1,27 @@
 import App from './app';
 
 const AppClass: App = new App();
-
 const app = AppClass.app;
+let port = AppClass.port;
 
-try {
-    app.listen(AppClass.port, () => {
+const startServer = () => {
+    app.listen(port, () => {
         console.log(
             `😎 [ED-API]: API is Running 🏁🏁🏁 http://localhost:${AppClass.port} 🏁🏁🏁 ✔`,
         );
+    }).on('error', (err: any) => {
+        if (err.code === 'EADDRINUSE') {
+            console.log(
+                `Port ${port} is already in use. Trying another port...`,
+            );
+            port += 1;
+            startServer();
+        } else {
+            console.log(err);
+        }
     });
-} catch (error) {
-    console.log(error);
-}
+};
+
+startServer();
 
 export default app;
