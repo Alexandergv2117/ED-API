bd3c15cac3a0d4df80ed4c9fa00b0b25c64777fb
Alexander Garcia <74172014+Alexandergv2117@users.noreply.github.com>
2023-03-11 23:08:57 -0500

    Se arreglo el bug de la conexión a la DB (#5)
    * add rute List of students by group
    
    * add all rutes of coordinator
    
    * Falla de conexión al realizar una segunda petición arreglada (#4)
    
    ---------
    
    Co-authored-by: langelX <langelxobl@gmail.com>
    Co-authored-by: Luis Angel xool balam <85704920+LangelXoBl@users.noreply.github.com>

--- src/models/coordinator.model.ts
+++ src/models/coordinator.model.ts
@@ -12,12 +12,6 @@ export default class Coordinator {
                 console.log(error);
                 return [];
             })
-            .finally(() => {
-                /*
-                Cerrar la conexión a la base de datos después de cada solicitud ayuda a evitar problemas como la saturación de conexiones y también es una buena práctica de seguridad para evitar conexiones innecesarias y no deseadas a la base de datos.
-                */
-                db.closeConnection();
-            });
     }
 
     public async getGroupsByProfesor(profesorId: number) {
@@ -31,9 +25,6 @@ export default class Coordinator {
                 console.log(error);
                 return [];
             })
-            .finally(() => {
-                db.closeConnection();
-            });
     }
 
     public async getProfesorsAverageByPeriod(periodId: number) {
@@ -47,8 +38,41 @@ export default class Coordinator {
                 console.log(error);
                 return [];
             })
-            .finally(() => {
-                db.closeConnection();
-            });
+    }
+    public async getAverageGroupByCarrer(careerId: number) {
+        return db.sequelize
+            .query('CALL getAverageGroupByCarrer(?)', {
+                replacements: [careerId],
+                type: 'RAW',
+            })
+            .then((res) => res)
+            .catch((error) => {
+                console.log(error);
+                return [];
+            })
+    }
+    public async getAverageQuestionByGroup(groupId: number) {
+        return db.sequelize
+            .query('CALL getAverageQuestionByGroup(?)', {
+                replacements: [groupId],
+                type: 'RAW',
+            })
+            .then((res) => res)
+            .catch((error) => {
+                console.log(error);
+                return [];
+            })
+    }
+    public async getStudentsByGroup(groupId: number) {
+        return db.sequelize
+            .query('CALL getStudentsByGroup(?)', {
+                replacements: [groupId],
+                type: 'RAW',
+            })
+            .then((res) => res)
+            .catch((error) => {
+                console.log(error);
+                return [];
+            })
     }
 }
