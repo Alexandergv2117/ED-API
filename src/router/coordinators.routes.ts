90631a9a0df7a9f36112e644ab9103c52b8b9839
langelX <langelxobl@gmail.com>
2023-03-11 20:06:20 -0500

    add all rutes of coordinator

--- src/router/coordinators.routes.ts
+++ src/router/coordinators.routes.ts
@@ -21,12 +21,27 @@ export default class CoordinatorRoutes {
         router.get('/', (req, res) => {
             res.send('Hello World!');
         });
+        // master rute
+        // periodos por carrera 1
         router.get('/periods/:careerId', Controller.getPeriodsByCareer);
+        // grupos por profesor 3
         router.get('/groups/:profesorId', Controller.getGroupsByProfesor);
+        // profesores por periodo 2
         router.get(
             '/profesors/average/:periodId',
             Controller.getProfesorsAverageByPeriod,
         );
+        // grupos por carrera 4
+        router.get(
+            '/groups/average/:careerId',
+            Controller.getAverageGroupByCarrer,
+        );
+        //promedio de preguntas por grupo 5
+        router.get(
+            '/questions/average/:groupId',
+            Controller.getAverageQuestionByGroup,
+        );
+        //lista de alumnos por grupo 6
         router.get('/student/list/:groupId', Controller.getStudentsByGroup);
     }
 }
